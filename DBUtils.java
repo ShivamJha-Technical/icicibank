6354c5368b7d78102eba5658ffc7c6002c20457b
ShivamJha97 <shivamkumar7493093@gmail.com>
2025-03-29 06:22:23 +0530

    reverted..Revert "added new file"
    This reverts commit cf419354c7bba6ceabf8284e167b63494bff3525.

--- DBUtils.java
+++ DBUtils.java
@@ -1,5 +1,3 @@
 This is database file...... Which contains some database files.
 
 Here i'm updating some contains
-
-Hello i'm updating again
--- test.c
+++
@@ -1,1 +0,0 @@
-Hello this file contains some C language code..
